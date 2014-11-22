6724b46799c3e12e5870293156336beba1d08b58
mammut02 <rod@MicMac-6.local>
2014-11-22 11:30:37 +0100

    Local notification

--- www/js/events/controllers/events.controller.js
+++ www/js/events/controllers/events.controller.js
@@ -9,53 +9,38 @@
         .module('ticketstracker.events.controllers')
         .controller('EventsController', EventsController);
 
-    EventsController.$inject = ['$scope', '$location', 'Events', 'Auth', '$ionicLoading', '$ionicModal'];
+    EventsController.$inject = ['$scope', 'Events', 'Auth', '$ionicLoading', '$ionicModal', '$cordovaLocalNotification'];
 
-    function EventsController($scope, $location, Events, Auth, $ionicLoading, $ionicModal) {
+    function EventsController($scope, Events, Auth, $ionicLoading, $ionicModal, $cordovaLocalNotification) {
         var vm = this;
         vm.change = change;
         vm.openModal = openModal;
         vm.closeModal = closeModal;
         vm.events = [];
 
+        $scope.login = login;
+        $scope.user = {
+            email: 'rodrigue.dev@gmail.com',
+            password: 'test'
+        };
+
         activate();
 
         function activate() {
             if (!Auth.signedIn()) {
-                //$location.path('/');
                 $ionicModal.fromTemplateUrl('templates/login.html', {
-                    scope: vm,
-                    animation: 'slide-in-up'
+                    scope: $scope,
+                    animation: 'slide-in-up',
+                    backdropClickToClose: false,
+                    focusFirstInput: true,
+                    hardwareBackButtonClose: false
                 }).then(function (modal) {
-                    console.log(modal);
                     vm.modal = modal;
-                    console.log(vm.modal);
+                    vm.openModal();
                 });
-
-                vm.openModal();
             }
             else {
-                $ionicLoading.show({
-                    template: '<i class="ion-loading-c"></i><br>Loading...'
-                });
-
-                vm.events = Events.all;
-                vm.events.$watch(function (event) {
-                    //console.log(event);
-                });
-
-                vm.events.$loaded()
-                    .then(function (theList) {
-                        $ionicLoading.hide();
-                    })
-                    .catch(function (error) {
-                        console.log("Error:", error);
-                    });
-
-                vm.$on('$destroy', function () {
-                    console.log('destroy');
-                    vm.modal.remove();
-                });
+                loadData();
             }
         }
 
@@ -64,13 +49,75 @@
         }
 
         function openModal() {
-            console.log('openModal');
             vm.modal.show();
-        };
+        }
 
         function closeModal() {
-            console.log('closeModal');
             vm.modal.hide();
-        };
+        }
+
+        function loadData() {
+            $ionicLoading.show({
+                template: '<i class="ion-loading-c"></i><br>Loading...'
+            });
+
+            vm.events = Events.all;
+
+            vm.events.$watch(function (event) {
+                //console.log(event);
+            });
+
+            vm.events.$loaded()
+                .then(function (theList) {
+                    $ionicLoading.hide();
+                })
+                .catch(function (error) {
+                    console.log("Error:", error);
+                });
+        }
+
+        function login(user) {
+            Auth.login(user).then(function (authData) {
+                console.log('login');
+                closeModal();
+                loadData();
+                addNotification();
+            }, function (error) {
+                console.log(error);
+            })
+        }
+
+        function addNotification() {
+
+            window.plugin.notification.local.hasPermission(function (granted) {
+                var now = new Date().getTime();
+                var milliseconds = (now + 10 * 1000);
+                var _10_seconds_from_now = new Date(milliseconds);
+
+                $cordovaLocalNotification.add({
+                    id: '1',
+                    message: 'id ' + milliseconds,
+                    title: 'Test',
+                    date: _10_seconds_from_now
+                }).then(function () {
+                    console.log('callback for adding background notification');
+                });
+            });
+
+        }
+
+        $scope.$on('notification_added', function (event, id, state, json) {
+            console.log('notification ADDED, id: ' + id + ' state:' + state + ' json:' + json);
+        });
+
+        $scope.$on('notification_triggered', function (event, id, state, json) {
+            console.log('notification TRIGGERED, id: ' + id + ' state:' + state + ' json:' + json);
+
+        });
+
+        $scope.$on('$destroy', function () {
+            console.log('destroy');
+            vm.modal.remove();
+        });
     }
 })();
--- www/js/ticketstracker.js
+++ www/js/ticketstracker.js
@@ -6,6 +6,7 @@
     angular
         .module('ticketstracker', [
             'ionic',
+            'ngCordova',
             'ticketstracker.routes',
             'ticketstracker.config',
             'ticketstracker.events',
@@ -23,9 +24,9 @@
         .run(run)
         .constant('FIREBASE_URL', 'https://fcb.firebaseio.com/');
 
-    run.$inject = ['$ionicPlatform'];
+    run.$inject = ['$ionicPlatform', '$rootScope'];
 
-    function run($ionicPlatform) {
+    function run($ionicPlatform, $rootScope) {
         $ionicPlatform.ready(function () {
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
@@ -36,6 +37,27 @@
                 // org.apache.cordova.statusbar required
                 StatusBar.styleDefault();
             }
+
+            window.plugin.notification.local.promptForPermission();
+
+            /*
+            window.plugin.notification.local.onadd = function (id, state, json) {
+                console.log('onadd ' + id);
+                $rootScope.$broadcast('notification_added', id, state, json);
+            };
+            window.plugin.notification.local.onclick = function (id, state, json) {
+                console.log('onclick');
+                $rootScope.$broadcast('notification_clicked', id, state, json);
+            };
+            window.plugin.notification.local.oncancel = function (id, state, json) {
+                console.log('oncancel');
+                $rootScope.$broadcast('notification_cancelled', id, state, json);
+            };
+            window.plugin.notification.local.ontrigger = function (id, state, json) {
+                console.log('ontrigger');
+                $rootScope.$broadcast('notification_triggered', id, state, json);
+            };
+            */
         });
     }
 })();
